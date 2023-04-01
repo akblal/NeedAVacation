5f671b3d561b5e34b948fe2a9ab402542cf5997b
Brandon Hsu <brandon.j.hsu@gmail.com>
2023-04-01 13:23:14 -0700

    skeleton structure for the app

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+function App () {
+
+  return (
+    <div>
+      I wanna get away
+
+    </div>
+  )
+}
+
+root.render(<App />)
